e930215605b068029213e82d3fd1dc6f637dbab7
gargdivyansh1 <divyanshgarg515@gmail.com>
2025-09-07 10:24:26 +0530

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Main from "./components/main/Main";
+const App = () => {
+  return (
+    <>
+      <Main/>
+    </>
+  )
+}
+
+
+export default App
+
+
+//  can add this for togging between languagews
+{/* <button className="bg-amber-700/30 hover:bg-amber-600/40 px-3 py-1 sm:px-4 sm:py-2 rounded-full text-xs sm:text-sm font-medium transition border border-amber-500/30 font-sans">
+    EN / हिंदी
+</button> */}
